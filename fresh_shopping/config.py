d16d01a168a3fd8f6a16ff2270d279d9978d6994
Wei Wei <wei@hem.com>
2019-08-14 13:51:25 +0200

    Adds django email backend for development
    - Send emails to django email backend in development
      enviroment.

--- fresh_shopping/config.py
+++ fresh_shopping/config.py
@@ -25,7 +25,12 @@ class Base:
 
 
 class Dev(Base):
-    pass
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    EMAIL_HOST = "127.0.0.1"
+    EMAIL_HOST_USER = ""
+    EMAIL_HOST_PASSWORD = ""
+    EMAIL_PORT = 1025
+    EMAIL_USE_TLS = False
 
 
 class Test(Base):
