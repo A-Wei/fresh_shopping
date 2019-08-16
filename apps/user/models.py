794d496da00a45b4d7ae8598d83c8fc48efb5431
Wei Wei <wei@hem.com>
2019-08-16 10:32:03 +0200

    Add AddressManager and user_center_info.html

--- apps/user/models.py
+++ apps/user/models.py
@@ -12,6 +12,16 @@ class User(AbstractUser, BaseModel):
         db_table = "fs_user"
 
 
+class AddressManager(models.Manager):
+    def get_default_address(self, user):
+        try:
+            address = self.get(user=user, is_default=True)
+        except self.model.DoesNotExist:
+            address = None
+
+        return address
+
+
 class Address(BaseModel):
     user = models.ForeignKey("User", on_delete=True)
     receiver = models.CharField(max_length=20)
@@ -20,5 +30,6 @@ class Address(BaseModel):
     phone = models.CharField(max_length=11)
     is_default = models.BooleanField(default=False)
 
+    objects = AddressManager()
     class Meta:
         db_table = "fs_address"
