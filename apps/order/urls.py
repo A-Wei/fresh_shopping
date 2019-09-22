796a0a8650a82e2d16e8fd22ae7ea2919e602bfd
Wei Wei <weiwei.alex@gmail.com>
2019-09-22 21:56:25 +0200

    Add place order
    - Add place_order.html
    - Add place order view
    - Config place order url

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from order.views import OrderPlaceView, OrderCommitView, OrderPayView, CheckPayView,CommentView
 
 urlpatterns = [
 
+    path('place', OrderPlaceView.as_view(), name='place'), # 提交订单信息
 ]
